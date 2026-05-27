89b56becff2b58c3ba40128e41b28e8aba2a0364
Pawan <Pawan@eccouncil.org>
2026-05-27 10:58:17 +0530

    added upstash redis url

--- backend/src/queue/connection.js
+++ backend/src/queue/connection.js
@@ -1,8 +1,36 @@
 const IORedis = require('ioredis');
 const config = require('../config');
 
+const redisUrl = new URL(config.redisUrl);
+const isTlsRedis =
+  redisUrl.protocol === 'rediss:' || redisUrl.hostname.endsWith('upstash.io');
+
 const connection = new IORedis(config.redisUrl, {
-  maxRetriesPerRequest: null
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+  ...(isTlsRedis ? { tls: {} } : {}),
+  retryStrategy(times) {
+    return Math.min(times * 300, 5000);
+  }
+});
+
+connection.on('connect', () => {
+  console.log('Redis: connected');
+});
+
+connection.on('ready', () => {
+  console.log('Redis: ready');
+});
+
+connection.on('reconnecting', () => {
+  console.warn('Redis: reconnecting');
+});
+
+connection.on('error', (error) => {
+  console.error('Redis: connection error', {
+    message: error.message,
+    code: error.code
+  });
 });
 
 module.exports = connection;
